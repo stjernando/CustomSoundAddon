956a65f3fc3dcc16d7c009dc073621e5c76d5f1e
Erik S <44195327+stjernando@users.noreply.github.com>
2020-11-13 19:19:39 +0100

    Update DaddysWargame.lua

--- DaddysWargame/Meat/DaddysWargame.lua
+++ DaddysWargame/Meat/DaddysWargame.lua
@@ -1,9 +1,6 @@
-message("Are you ready for Mad Matt's Wargames?!")
-PlaySoundFile("Interface\\AddOns\\DaddysWargame\\Lyder\\honk.ogg")
-
-
-
 local ADDON_ID = 'DaddysWargame'
+
+
 local BAM_SOUND = 'Interface/AddOns/DaddysWargame/Lyder/horse.ogg'
 local BAAAM_SOUND = 'Interface/AddOns/DaddysWargame/Lyder/honk.ogg'
 local alban = 'Interface/AddOns/DaddysWargame/Lyder/alban.ogg'
@@ -28,6 +25,27 @@ local sus = 'Interface/AddOns/DaddysWargame/Lyder/sus.ogg'
 local beer = 'Interface/AddOns/DaddysWargame/Lyder/beer.ogg'
 local kids = 'Interface/AddOns/DaddysWargame/Lyder/kids.ogg'
 local nice = 'Interface/AddOns/DaddysWargame/Lyder/nice.ogg'
+local blessyou = 'Interface/AddOns/DaddysWargame/Lyder/blessyou.ogg'
+local ohyeah = 'Interface/AddOns/DaddysWargame/Lyder/ohyeah.ogg'
+local yoo = 'Interface/AddOns/DaddysWargame/Lyder/yoo.ogg'
+local pudding = 'Interface/AddOns/DaddysWargame/Lyder/pudding.ogg' 
+local ty = 'Interface/AddOns/DaddysWargame/Lyder/ty.ogg' 
+local lesson = 'Interface/AddOns/DaddysWargame/Lyder/lesson.ogg' 
+local ymca = 'Interface/AddOns/DaddysWargame/Lyder/ymca.ogg' 
+local mariodeath = 'Interface/AddOns/DaddysWargame/Lyder/mariodeath.ogg' 
+
+
+local umad = 'Interface/AddOns/DaddysWargame/Lyder/umad.ogg' 
+local penis = 'Interface/AddOns/DaddysWargame/Lyder/penis.ogg' 
+local penisppl = 'Interface/AddOns/DaddysWargame/Lyder/penisppl.ogg' 
+local nuke = 'Interface/AddOns/DaddysWargame/Lyder/nuke.ogg' 
+local yeah = 'Interface/AddOns/DaddysWargame/Lyder/yeah.ogg' 
+local idkalban = 'Interface/AddOns/DaddysWargame/Lyder/idkalban' 
+local chick = 'Interface/AddOns/DaddysWargame/Lyder/chick.ogg' 
+local father = 'Interface/AddOns/DaddysWargame/Lyder/father.ogg' 
+local caramell = 'Interface/AddOns/DaddysWargame/Lyder/caramell.ogg' 
+local kid = 'Interface/AddOns/DaddysWargame/Lyder/kid.ogg' 
+
 
 local THROTTLE_SECONDS = 2
 
@@ -39,7 +57,7 @@ local function ends_with(str, ending)
   return ending == "" or str:sub(-#ending) == ending
 end
 
-local function printBam(...)
+ local function printBam(...)
   print('|cFF9BDBF5'..ADDON_TITLE)
 end
 
@@ -81,73 +99,157 @@ f:SetScript("OnEvent", function(self, event)
   self:OnEvent(event, CombatLogGetCurrentEventInfo())
 end)
 
+
+
+local frame = CreateFrame("FRAME");
+frame:RegisterEvent("PLAYER_DEAD");
+local function eventHandler(self, event, ...)
+  local dic = math.random(1,5)
+  if (dic == 1) then
+	PlaySoundFile([[Interface\AddOns\DaddysWargame\Lyder\yezyes.ogg]], "Master")
+	elseif (dic == 2) then
+		PlaySoundFile([[Interface\AddOns\DaddysWargame\Lyder\hellodark.ogg]], "Master")
+	elseif (dic == 3) then
+		PlaySoundFile([[Interface\AddOns\DaddysWargame\Lyder\fug.ogg]], "Master")
+	elseif (dic == 4) then
+		PlaySoundFile([[Interface\AddOns\DaddysWargame\Lyder\oof.ogg]], "Master")
+	elseif (dic == 5) then
+		PlaySoundFile([[Interface\AddOns\DaddysWargame\Lyder\mariodeath.ogg]], "Master")
+	end
+ 
+end
+
+
+frame:SetScript("OnEvent", eventHandler);
+
+local fr = CreateFrame("FRAME")
+fr:RegisterEvent("PLAYER_UNGHOST");
+local function eventHandler(self, event, ...)
+	PlaySoundFile([[Interface\AddOns\DaddysWargame\Lyder\return.ogg]], "Master")
+end
+fr:SetScript("OnEvent", eventHandler);
+
+local fr1 = CreateFrame("FRAME")
+fr1:RegisterEvent("PLAYER_REGEN_DISABLED");
+local function eventHandler(self, event, ...)
+	local x = math.random(1,12)
+	if (x == 1) then 
+		PlaySoundFile([[Interface\AddOns\DaddysWargame\Lyder\return.ogg]], "Master")
+	elseif (x == 2) then
+		PlaySoundFile(lesson, 'Master')
+	elseif (x == 3) then
+		PlaySoundFile(ymca, 'Master')
+	elseif (x == 4) then
+		PlaySoundFile(umad, 'Master')
+	elseif (x == 5) then
+		PlaySoundFile(penis, 'Master')
+	elseif (x == 6) then
+		PlaySoundFile(penisppl, 'Master')
+	elseif (x == 7)then
+		PlaySoundFile(nuke, 'Master')
+	elseif (x == 8) then
+		PlaySoundFile(yeah, 'Master')
+	elseif (x == 9) then
+		PlaySoundFile(idkalban, 'Master')
+	elseif (x == 10) then
+		PlaySoundFile(chick, 'Master')
+	elseif (x == 11) then
+		PlaySoundFile(father, 'Master')
+	elseif (x == 12) then
+		PlaySoundFile(caramell, 'Master')
+	elseif (x == 13) then
+		PlaySoundFile(kid, 'Master')
+	end
+end
+fr1:SetScript("OnEvent", eventHandler);
+
 function f:OnEvent(event, ...)
   local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
   local spellId, spellName, spellSchool
   local environmentalType
   local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand
-  
+	
   if (subevent == "SWING_DAMAGE") then
     amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
   elseif (subevent == "ENVIRONMENTAL_DAMAGE") then
     environmentalType, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+  elseif (subevent == "SPELL_HEAL") then
+	spellId, spellName, spellSchool, amount, overheal, absorbed, critical = select(12, ...)
   else  
     spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
   end
   
+if (subevent == "SPELL_HEAL" and critical and sourceGUID == PLAYER_GUID) then
+	local terning = math.random(1,7)
+	if (terning == 1) then
+		PlaySoundFile(blessyou, 'Master')
+		elseif (terning == 2) then
+			PlaySoundFile(ohy, 'Master')
+		elseif (terning == 3) then
+			PlaySoundFile(yoo, 'Master')
+		elseif (terning == 4) then
+			PlaySoundFile(pudding, 'Master')	
+		elseif (terning == 5) then
+			PlaySoundFile(ohyes, 'Master')	
+		elseif (terning == 6) then
+			PlaySoundFile(borat, 'Master')	
+		elseif (terning == 7) then
+			PlaySoundFile(ty, 'Master')	
+	end
+end
   if (ends_with(subevent, '_DAMAGE') and critical and sourceGUID == PLAYER_GUID) then
-    if (BamCharSettings['TRIGGER_ON_SWING'] or spellId) then
-      if (overkill and overkill > 0) and BamCharSettings['BAAAM_ON_OVERKILL'] then
-        if (BamCharSettings['THROTTLE_SOUNDS']) then
-          throttledBaaamSound()
-        else
-		local t = math.random(1,22)
-		if (t ==1 ) then
-				PlaySoundFile(BAAAM_SOUND, 'Master')
-			elseif (t ==2 ) then
-				PlaySoundFile(BAM_SOUND, 'Master')
-			elseif (t ==3 ) then
-				PlaySoundFile(alban, 'Master')
-			elseif (t ==4 ) then
-				PlaySoundFile(boing, 'Master')
-			elseif (t ==5 ) then
-				PlaySoundFile(boing2, 'Master')
-			elseif (t ==6 ) then
-				PlaySoundFile(boing3, 'Master')
-			elseif (t ==7 ) then
-				PlaySoundFile(boing4, 'Master')
-			elseif (t ==8 ) then
-				PlaySoundFile(borat, 'Master')
-			elseif (t ==9 ) then
-				PlaySoundFile(bowling, 'Master')
-			elseif (t ==10 ) then
-				PlaySoundFile(drunk1, 'Master')
-			elseif (t ==11 ) then
-				PlaySoundFile(slap, 'Master')
-			elseif (t ==12 ) then
-				PlaySoundFile(woop, 'Master')
-			elseif (t ==13 ) then
-				PlaySoundFile(woopwoop, 'Master')
-			elseif (t ==14 ) then
-				PlaySoundFile(ele, 'Master')
-			elseif (t ==15 ) then
-				PlaySoundFile(huw, 'Master')	
-			elseif (t ==16 ) then
-				PlaySoundFile(pig, 'Master')	
-			elseif (t ==17 ) then
-				PlaySoundFile(ohyes, 'Master')
-			elseif (t ==18 ) then
-				PlaySoundFile(mario, 'Master')
-			elseif (t ==19 ) then
-				PlaySoundFile(sus, 'Master')
-			elseif (t ==20 ) then
-				PlaySoundFile(beer, 'Master')
-			elseif (t ==21 ) then
-				PlaySoundFile(kids, 'Master')
-			elseif (t ==22 ) then
-				PlaySoundFile(nice, 'Master')	
+		if (BamCharSettings['TRIGGER_ON_SWING'] or spellId) then
+		  if (overkill and overkill > 0) and BamCharSettings['BAAAM_ON_OVERKILL'] then
+			if (BamCharSettings['THROTTLE_SOUNDS']) then
+			  throttledBaaamSound()
+			else
+			local t = math.random(1,22)
+			if (t ==1 ) then
+					PlaySoundFile(BAAAM_SOUND, 'Master')
+				elseif (t ==2 ) then
+					PlaySoundFile(BAM_SOUND, 'Master')
+				elseif (t ==3 ) then
+					PlaySoundFile(alban, 'Master')
+				elseif (t ==4 ) then
+					PlaySoundFile(boing, 'Master')
+				elseif (t ==5 ) then
+					PlaySoundFile(boing2, 'Master')
+				elseif (t ==6 ) then
+					PlaySoundFile(boing3, 'Master')
+				elseif (t ==7 ) then
+					PlaySoundFile(boing4, 'Master')
+				elseif (t ==8 ) then
+					PlaySoundFile(borat, 'Master')
+				elseif (t ==9 ) then
+					PlaySoundFile(bowling, 'Master')
+				elseif (t ==10 ) then
+					PlaySoundFile(drunk1, 'Master')
+				elseif (t ==11 ) then
+					PlaySoundFile(slap, 'Master')
+				elseif (t ==12 ) then
+					PlaySoundFile(woop, 'Master')
+				elseif (t ==13 ) then
+					PlaySoundFile(woopwoop, 'Master')
+				elseif (t ==14 ) then
+					PlaySoundFile(ele, 'Master')
+				elseif (t ==15 ) then
+					PlaySoundFile(huw, 'Master')	
+				elseif (t ==16 ) then
+					PlaySoundFile(pig, 'Master')	
+				elseif (t ==17 ) then
+					PlaySoundFile(ohyes, 'Master')
+				elseif (t ==18 ) then
+					PlaySoundFile(mario, 'Master')
+				elseif (t ==19 ) then
+					PlaySoundFile(sus, 'Master')
+				elseif (t ==20 ) then
+					PlaySoundFile(beer, 'Master')
+				elseif (t ==21 ) then
+					PlaySoundFile(kids, 'Master')
+				elseif (t ==22 ) then
+					PlaySoundFile(nice, 'Master')	
+				end
 			end
-        end
       else
         if (BamCharSettings['THROTTLE_SOUNDS']) then
           throttledBamSound()
@@ -199,12 +301,12 @@ function f:OnEvent(event, ...)
 				PlaySoundFile(nice, 'Master')	
 			
 			end
-			
         end
       end
     end
   end
 end
+
 
 SLASH_BAM1 = '/harderdaddy'
 SlashCmdList['BAM'] = function(command)
@@ -223,7 +325,7 @@ SlashCmdList['BAM'] = function(command)
     printBam('Config')
     print('  Plays effects! sounds every time you crit!|n|n')
     print('/daddy swing|n-- Toggle Sound trigger on Weapon(Swing) crits.', 'Currently', formatSetting(BamCharSettings['TRIGGER_ON_SWING']))
-    print('/daddy overkill|n-- Toggle Different Baaam! sound for Overkill crits.', 'Currently', formatSetting(BamCharSettings['BAAAM_ON_OVERKILL']))
+    print('/daddy overkill|n-- Toggle Differen sound for Overkill crits.', 'Currently', formatSetting(BamCharSettings['BAAAM_ON_OVERKILL']))
     print('/daddy throttle|n-- Toggle Only play sounds every 2 seconds!', 'Currently', formatSetting(BamCharSettings['THROTTLE_SOUNDS']))
   end
 end
